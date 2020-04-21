233fd6de19fea6411efce8c156b17943fe44eb3a
Brandon J <splintercel3000@gmail.com>
2020-04-21 15:31:03 -0700

    finished the user story for registering.

---
+++ HoleyForkingShirt/HoleyForkingShirt/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using HoleyForkingShirt.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoleyForkingShirt.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ HoleyForkingShirt/HoleyForkingShirt/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoleyForkingShirt.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LatName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
